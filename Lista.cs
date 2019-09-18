21b5054fdf196715c247d37a65226a5fc281d977
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2019-09-17 21:26:49 -0300

    Fin

--- Lista.cs
+++ Lista.cs
@@ -6,13 +6,14 @@ namespace ABMLista.Clases
     {
         #region Propiedades
         private string[] lista = new string[1];
+        private string[] listanota = new string[1];
         private int ProximaPosicion = 0;
         #endregion
 
         #region Constructor
         #endregion
         #region Metodos
-        public bool Agregar(string aTexto)
+        public bool Agregar(string aTexto,string anota)
         {
             bool Resp = false;
             try
@@ -20,9 +21,11 @@ namespace ABMLista.Clases
                 if (ProximaPosicion == lista.Length)
                 {
                     this.AgregaRegistro(1);
+                    this.AgregaRegistronota(1);
                 }
 
                 lista[ProximaPosicion] = aTexto;
+                listanota[ProximaPosicion] = anota;
                 ProximaPosicion++;
                 Resp = true;
             }
@@ -34,10 +37,16 @@ namespace ABMLista.Clases
             return Resp;
         }
 
-        private void AgregaRegistro(int incremento)
+        public void AgregaRegistro(int incremento)
         {
             string[] Temp = new string[lista.Length + incremento];
             lista = this.Copiar(lista, Temp);
+        }
+        public void AgregaRegistronota(int incremento)
+        {
+
+            string[] Temp2 = new string[listanota.Length + incremento];
+            listanota = this.Copiarnota(listanota, Temp2);
 
         }
         private string[] Copiar(string[] Origen, string[] Destino)
@@ -48,18 +57,30 @@ namespace ABMLista.Clases
             }
             return Destino;
         }
+        private string[] Copiarnota(string[] Origen, string[] Destino)
+        {
+            for (int i = 0; i < ProximaPosicion; i++)
+            {
+                Destino[i] = Origen[i];
+            }
+            return Destino;
+        }
         public string MostrarLista()
         {
             string Respuesta = "";
+            string resp1 = "";
             if (ProximaPosicion > 0)
             {
-                Respuesta = lista[0];
+                Respuesta = lista[0]+listanota[0];
+                resp1 = listanota[0];
                 for (int i = 1; i < ProximaPosicion; i++)
                 {
-                    Respuesta = Respuesta + "\r\n" + lista[i];
+                    Respuesta = Respuesta +" " + lista[i]+ " "+ listanota[i]+ "\r\n";
+                    
                 }
             }
             return Respuesta;
+            return resp1;
         }
         public int BuscarPosicion(string Que)
         {
@@ -72,7 +93,11 @@ namespace ABMLista.Clases
                     Resp = i;
                     break;
                 }
-
+                if (listanota[i].CompareTo(Que) == 0)
+                {
+                    Resp = i;
+                    break;
+                }
 
             }
             return Resp;
@@ -90,19 +115,24 @@ namespace ABMLista.Clases
                 for (int i = Pos; i < ProximaPosicion - 1; i++)
                 {
                     this.lista[i] = this.lista[i + 1];
+                    this.listanota[i] = this.listanota[i + 1];
                 }
                 this.lista[ProximaPosicion - 1] = null;
+                this.listanota[ProximaPosicion - 1] = null;
                 ProximaPosicion = ProximaPosicion - 1;
                 AgregaRegistro(-1);
+                AgregaRegistronota(-1);
             }
 
             return Resp;
         }
         public string Ordenar()
         {
-            string salida = "";
+            string finalizado = "";
             string[] copia = new string[lista.Length];
+            string[] copia2 = new string[listanota.Length];
             Copiar(lista, copia);
+            Copiarnota(listanota, copia2);
             if (copia.Length > 1)
             {
                 for (int j = 0; j < copia.Length; j++)
@@ -117,7 +147,9 @@ namespace ABMLista.Clases
                                 string Temp = copia[i];
                                 copia[i] = copia[i + 1];
                                 copia[i + 1] = Temp;
-
+                                string Temp2 = copia2[i];
+                                copia2[i] = copia[i + 1];
+                                copia2[i + 1] = Temp2;
                             }
                         }
                         catch (IndexOutOfRangeException)
@@ -130,10 +162,13 @@ namespace ABMLista.Clases
             }
             for (int i = 0; i < copia.Length; i++)
             {
-                salida = salida + copia[i] + "\r\n";
+                finalizado = finalizado + copia[i] + "  "+ copia2[i]+ "\r\n";
             }
-
-            return salida;
+            //for (int i = 0; i < copia2.Length; i++)
+            //{
+            //    finalizado = finalizado + copia2[i];
+            //}
+            return finalizado;
         }
         #endregion
     }
